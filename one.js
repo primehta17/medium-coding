cbf95d6411b97162c74bfe280d9dde5b7cf71eeb
Priyanka Mehta <primehta17@gmail.com>
2025-05-13 23:40:01 +0530

    merge and sort

--- one.js
+++ one.js
@@ -1,9 +1,27 @@
-function checkSimilarArray(arr1, arr2) {
-    console.log(arr2)
-    if(arr1.sort()!=arr2.sort()){
-       return "similar"
-     
-  } return "Not Similar"
- 
+let arr1 = [1, 4, 5, 7, 9];
+let arr2 = [2, 3, 6];
+
+function merge(arr1, arr2) {
+    let arr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        arr.push(arr1[i]);
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        arr.push(arr2[i]);
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    return arr;
 }
-console.log(checkSimilarArray([1,2,3], [3,2,1]))
+
+console.log(merge(arr1, arr2)); // [1, 2, 3, 4, 5, 6, 7, 9]
+console.log("Try programiz.pro");
