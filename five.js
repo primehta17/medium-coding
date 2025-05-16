c9fb75bd66d6448c1635f0410c9fcd2c0c5c9afe
Priyanka Mehta <primehta17@gmail.com>
2025-05-16 12:51:39 +0530

    difference between max and min

---
+++ five.js
@@ -0,0 +1,19 @@
+let arr=[-1,-2,-3,-4,-5];
+let arr2=[-10, 20, 30, -40]
+
+
+function findNegativeRange(arr) {
+      let min=arr[0]; let max=arr[0];
+      for(let i=0;i<arr.length;i++){
+        if(min>arr[i]){
+            min=arr[i];
+        }
+        if(max<arr[i]){
+            max=arr[i];
+        }
+        
+      }
+      return max-min;
+}
+console.log(findNegativeRange(arr))
+console.log(findNegativeRange(arr2))
