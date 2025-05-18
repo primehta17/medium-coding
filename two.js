a37f5e4d027e00b1a476417a69948db84ca6b221
Priyanka Mehta <primehta17@gmail.com>
2025-05-19 00:19:16 +0530

    set value

--- two.js
+++ two.js
@@ -1,4 +1,6 @@
 
+//1
+
 function setelement(str){
   let newset = new Set(str);
   
@@ -7,9 +9,26 @@ return [...newset];
 }
 console.log(setelement("geeksofgeeks"));
 
+
+//2
 function uniquevalue(str){
   let newset=new Set(str)
   const uniqueCharsArray = Array.from(newset);
   return uniqueCharsArray;
 }
 console.log(uniquevalue("geeksofgeeks"));
+
+//3
+let arr = new Set([1,2,2,3]);
+console.log(arr);
+
+//4
+let newarr = new Set();
+newarr.add(1);
+newarr.add(2);
+newarr.delete(1);
+newarr.clear();
+
+console.log(newarr);
+
+
