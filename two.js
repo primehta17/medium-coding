26ed796690924b5a78206e2b9814b6fe57cf1815
Priyanka Mehta <primehta17@gmail.com>
2025-05-19 00:09:27 +0530

    set value in array

--- two.js
+++ two.js
@@ -1,7 +1,15 @@
 
 function setelement(str){
   let newset = new Set(str);
+  
 return [...newset];
 
 }
-console.log(setelement("geeksofgeeks"))
+console.log(setelement("geeksofgeeks"));
+
+function uniquevalue(str){
+  let newset=new Set(str)
+  const uniqueCharsArray = Array.from(newset);
+  return uniqueCharsArray;
+}
+console.log(uniquevalue("geeksofgeeks"));
