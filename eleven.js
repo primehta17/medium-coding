cb088ea98b1e34e57679df81aee02147fb0d7881
Priyanka Mehta <primehta17@gmail.com>
2025-05-21 00:23:34 +0530

    largest number

--- eleven.js
+++ eleven.js
@@ -81,4 +81,15 @@ function vowels(str){
     }
     return count;
 }
-console.log(vowels("Hello World!"))
+console.log(vowels("Hello World!"));
+
+function largestnum(arr){
+    let max=arr[0];
+    for(let i=0;i<arr.length;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+console.log(largestnum([3,32,44,35]))
