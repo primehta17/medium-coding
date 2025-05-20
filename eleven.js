7dc9815c5131c8a0d5ebdedf8a272a445624f5ca
Priyanka Mehta <primehta17@gmail.com>
2025-05-21 00:30:25 +0530

    factorial number

--- eleven.js
+++ eleven.js
@@ -106,4 +106,14 @@ function prime(num){
    }
    return true;
 }
-console.log(prime(21))
+console.log(prime(21));
+
+
+function factorial(num){
+    let fact=1;
+    for(let i=1;i<=num;i++){
+      fact*=i;
+    }
+    return fact;
+}
+console.log(factorial(5))
