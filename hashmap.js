b824ba79d6b1110defcc7a21ff2a9a5f33c350db
Priyanka Mehta <primehta17@gmail.com>
2025-06-02 23:01:39 +0530

    other ways towrite map

--- hashmap.js
+++ hashmap.js
@@ -53,3 +53,33 @@ for(const pair of mapiterate){
     console.log(`${key}: ${value}`);
     console.log("+++++++++++++++ pair", pair);
 }
+
+
+
+// Other ways to write map
+//1
+const map1 = new Map([['a',1],['b',2]]);
+//2
+const map2 = new Map();
+map2.set('a',1);
+map2.set('b',2);
+//3
+const map3 = {a:1,b:2};
+const newmap = new Map(Object.entries(map3));
+console.log(newmap);
+//4
+const entries = [['a', 1], ['b', 2]];
+const map4 = new Map(entries);
+console.log(map4);
+
+//5
+const arr = [{key: 'a', value:1},{key: 'b', value:2}];
+const map5 = new Map(arr.map(({key, value}) => [key, value]));
+console.log(map5);
+
+//6
+const arrtwo = [{key: 'a', value:1},{key: 'b', value:2}];
+const map6 = arrtwo.reduce((acc, {key, value}) => acc.set(key, value), new Map());
+console.log(map6);
+
+
