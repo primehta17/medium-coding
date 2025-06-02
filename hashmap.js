9f0a18291e97b35022d623f7c18f9b4bb125f007
Priyanka Mehta <primehta17@gmail.com>
2025-06-02 22:37:03 +0530

    hashmap iterate

--- hashmap.js
+++ hashmap.js
@@ -11,3 +11,45 @@ map.set('1','String key');
 map.set(1, 'number key');
 console.log(map.get('1'));
 console.log(map.get(1));
+
+
+// Use the has() method to determine if a key exists in the Map.
+
+const maphas = new Map([['a',1]]);
+console.log(maphas.has('a'));
+console.log(maphas.has('b'));
+
+// add a key that already exists in the Map, the new value will 
+// overwrite the existing value associated with that key.
+
+const mapduplicate = new Map();
+mapduplicate.set('a',1);
+mapduplicate.set('a',2);
+console.log(mapduplicate.get('a'));
+
+
+// Use the delete() method to remove a specific key-value pair from the Map.
+
+const mapdelete = new Map();
+
+mapdelete.set('a','qwerty');
+mapdelete.set('b',2);
+
+mapdelete.delete('a');
+
+console.log(mapdelete.has('a'));
+
+// iterate over a Map
+
+const mapiterate = new Map([['a',1],['b',2]]);
+console.log("+++++++++++ new map", mapiterate);
+
+//using the default iterator of Map
+for(const pair of mapiterate){
+    
+    const key = pair[0];
+    const value = pair[1];
+
+    console.log(`${key}: ${value}`);
+    console.log("+++++++++++++++ pair", pair);
+}
--- hashmaphas.js
+++
@@ -1,5 +0,0 @@
-// Use the has() method to determine if a key exists in the Map.
-
-const map = new Map([['a',1]]);
-console.log(map.has('a'));
-console.log(map.has('b'));
