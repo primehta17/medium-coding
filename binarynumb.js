284eb7c490cb166d8e09c832c1ea6c4854bcff81
Priyanka Mehta <primehta17@gmail.com>
2025-06-05 23:46:51 +0530

    binarytodecimal

--- binarynumb.js
+++ binarynumb.js
@@ -1,9 +1,21 @@
 function binaryToDecimal(binary) {
     let decimal =0;
     for(let i=0;i<binary.length;i++){
-         decimal = decimal * 2 + Number(binary[i]);
+        decimal = decimal *2 + Number(binary[i]);
     }
     return decimal;
 }
+console.log(binaryToDecimal('1010'));
 
-console.log(binaryToDecimal('1010'))
+
+
+function decimalToBinary(num){
+  if(num===0) return 0;
+  let binary ='';
+    while(num>0){
+        binary = (num%2)+binary;
+        num = Math.floor(num/2);
+    }
+    return binary;
+}
+console.log(decimalToBinary(10))
