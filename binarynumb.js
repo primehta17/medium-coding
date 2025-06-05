12cdcee9083d7d17150ddbdbfd2727a5158d961a
Priyanka Mehta <primehta17@gmail.com>
2025-06-05 23:59:35 +0530

    stringcount

--- binarynumb.js
+++ binarynumb.js
@@ -6,16 +6,3 @@ function binaryToDecimal(binary) {
     return decimal;
 }
 console.log(binaryToDecimal('1010'));
-
-
-
-function decimalToBinary(num){
-  if(num===0) return 0;
-  let binary ='';
-    while(num>0){
-        binary = (num%2)+binary;
-        num = Math.floor(num/2);
-    }
-    return binary;
-}
-console.log(decimalToBinary(10))
---
+++ sortstringlist.js
@@ -0,0 +1,8 @@
+function sortStringList(strList) {
+    for(let i=0;i<strList.length;i++){
+        console.log(strList[i].length)
+    }
+    
+}
+let strList=["carrot", "pea", "pumpkin"];
+console.log(sortStringList(strList) )
