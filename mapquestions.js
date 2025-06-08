a975fe1f45861836b65894f75c399b70f6c7cac6
Priyanka Mehta <primehta17@gmail.com>
2025-06-09 00:07:25 +0530

    get the frequency in map

--- mapquestions.js
+++ mapquestions.js
@@ -6,8 +6,17 @@ map.set("3","orange");
 map.set(4,"berry");
 map.set('contact',[976754344, 3424563])
 
-console.log(map);
+// console.log(map);
 
-for(let i=0;i<map.length;i++){
 
+let arr = [10,3,5,6,10,3,5,5];
+let newmap= new Map();
+for(let i=0;i<arr.length;i++){
+//    if(newmap.has(arr[i])){
+//         newmap.set(arr[i], newmap.get(arr[i]) + 1);
+//    }else{
+//         newmap.set(arr[i], 1);
+//    }
+newmap.set(arr[i], (newmap.get(arr[i])||0)+1)
 }
+console.log(newmap)
