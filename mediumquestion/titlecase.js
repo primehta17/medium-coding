dafef0dec033a439a37fca1146b355ebbccd34e2
Priyanka Mehta <primehta17@gmail.com>
2025-06-15 19:36:26 +0530

    titlecase

--- mediumquestion/titlecase.js
+++ mediumquestion/titlecase.js
@@ -6,4 +6,13 @@ function convertToTitleCase(str) {
    .toUpperCase() + word.slice(1))
    .join(' ');
 }
-console.log(convertToTitleCase("learn with program"))
+console.log(convertToTitleCase("learn with program"));
+
+function converttotitle(str){
+    let arr = str.toLowerCase().split(' ');
+    for(let i=0;i<arr.length;i++){
+       arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+return arr.join(' ');
+}
+console.log(converttotitle("learn the code"))
