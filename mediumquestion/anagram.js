e90f8e604135b43f9bd6557f8a86dcd0c9dde446
Priyanka Mehta <primehta17@gmail.com>
2025-06-16 23:45:50 +0530

    anagram

---
+++ mediumquestion/anagram.js
@@ -0,0 +1,11 @@
+function isAnagram(str1, str2) {
+    if (str1.length !== str2.length) return false;
+
+    const sorted1 = str1.split('').sort().join('');
+    const sorted2 = str2.split('').sort().join('');
+
+    return sorted1 === sorted2;
+}
+
+console.log(isAnagram("listen", "silent")); // true
+console.log(isAnagram("hello", "world"));   // false
