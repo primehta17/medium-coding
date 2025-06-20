f704b8b82034ca78f6e47a020d6d395cdeaf474b
Priyanka Mehta <primehta17@gmail.com>
2025-06-20 23:56:22 +0530

    calculatedivisorss

---
+++ mediumquestion/calculateDivisors.js
@@ -0,0 +1,10 @@
+function calculateDivisors(num) {
+    let count=0;
+   for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+   }
+   return count;
+}
+console.log(calculateDivisors(12));
