3578d40cceef920ca187818556c2267ac69fc470
Priyanka Mehta <primehta17@gmail.com>
2025-06-21 01:32:02 +0530

    gin sort optimize

--- mediumquestion/ginort.js
+++ mediumquestion/ginort.js
@@ -19,3 +19,24 @@ function ginsort(strs){
 }
 console.log(ginsort('Sorting4321'));
 
+function ginortSOrder(s) {
+  const lower = [], upper = [], odd = [], even = [];
+
+  for (const ch of s) {
+    if (ch >= 'a' && ch <= 'z') lower.push(ch);
+    else if (ch >= 'A' && ch <= 'Z') upper.push(ch);
+    else if (ch >= '0' && ch <= '9') {
+      if (parseInt(ch) % 2) odd.push(ch);
+      else even.push(ch);
+    }
+  }
+
+  return (
+    lower.sort().join('') +
+    upper.sort().join('') +
+    odd.sort().join('') +
+    even.sort().join('')
+  );
+}
+
+console.log(ginortSOrder('Sorting4321'));
