b34941e9073003b9cac2826779805ffcb3bb17e3
Priyanka Mehta <primehta17@gmail.com>
2025-06-22 01:27:17 +0530

    switchcase

--- mediumquestion/switchcase.js
+++ mediumquestion/switchcase.js
@@ -6,3 +6,13 @@ switch(true){
     default:
         console.log("invalid");
 }
+
+let num=0.1+0.2;//0.30000006
+switch(num){
+    case 0.3: console.log("hello");
+    break;
+    case 0.5: console.log("hey");
+    break;
+    default:
+        console.log("invalid");
+}
