cf0b64569c1c6d6baf0683a6ccdf8a24b3d16237
Priyanka Mehta <primehta17@gmail.com>
2025-06-22 01:29:59 +0530

    switchcase

--- mediumquestion/switchcase.js
+++ mediumquestion/switchcase.js
@@ -8,6 +8,12 @@ switch(true){
 }
 
 let num=0.1+0.2;//0.300000000006
+// Using num.toFixed(1) gives only 1 digit after the decimal point. 
+// But as this provides value in String using Number makes it a number.
+//  So now the number 0.30000000000000004 it gives 0.3. 
+// which will match our case and prints Hello for us.
+
+
 // switch(num){
 switch (Number(num.toFixed(1))){
     case 0.3: console.log("hello");
