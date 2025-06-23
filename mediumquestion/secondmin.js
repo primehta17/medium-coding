5fe0738fce9c4ded4fd64977b2ad23f13611101e
Priyanka Mehta <primehta17@gmail.com>
2025-06-23 20:51:08 +0530

    second minimum

---
+++ mediumquestion/secondmin.js
@@ -0,0 +1,14 @@
+function secondmin(arr){
+    let min=Math.min(arr[0],arr[1]);
+    let secondmin=Math.max(arr[0],arr[1]);
+    for(let i=2;i<arr.length;i++){
+        if(arr[i]<min){
+            secondmin=min;
+            min=arr[i];
+        }else if(arr[i]<secondmin){
+            secondmin=arr[i];
+        }
+    }
+    return secondmin;
+}
+console.log(secondmin([2,33,4,3,22,21]));
