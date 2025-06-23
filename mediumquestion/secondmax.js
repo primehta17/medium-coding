d7b4803c0d80e428c798b6c429ed01e6abb67b38
Priyanka Mehta <primehta17@gmail.com>
2025-06-23 20:45:17 +0530

    second max sh

--- mediumquestion/secondmax.js
+++ mediumquestion/secondmax.js
@@ -3,7 +3,7 @@ function secondmax(arr){
     for(let i=0;i<arr.length;i++){
         if(arr[i]>max){
             secondmax=max;
-            console.log(max)
+            // console.log(max)
             max=arr[i];
         }   else if(arr[i]<max && arr[i]>secondmax){
             secondmax=arr[i];
@@ -12,4 +12,21 @@ function secondmax(arr){
      return secondmax;
 }
 let arr=[3,4,5,2,,6,9];
-console.log(secondmax(arr));
+// console.log(secondmax(arr));
+
+
+function secondshmax(arr){
+    let max=Math.max(arr[0],arr[1]);
+    let secondmax=Math.min(arr[0],arr[1]);
+
+    for(let i=2;i<arr.length;i++){
+        if(arr[i]>max){
+            secondmax=max;
+            max=arr[i];
+        }else if(arr[i]>secondmax){
+            secondmax=arr[i];
+        }
+    }
+    return secondmax;
+}
+console.log(secondshmax(arr));
