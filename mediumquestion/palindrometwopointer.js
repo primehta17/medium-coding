8c5f54b4a744674fe6815a33082c583fb4c1542a
Priyanka Mehta <primehta17@gmail.com>
2025-06-26 00:13:19 +0530

    palindrone two pinter

--- mediumquestion/palindrometwopointer.js
+++ mediumquestion/palindrometwopointer.js
@@ -2,13 +2,23 @@ function twopointerpalindrom(str){
     let start=0;
     let end=str.length-1;
     while(start<end){
-        if(str[start] !== str[end]){
+        let lower=str[start].toLowerCase();
+      let high=str[end].toLowerCase();
+      if(lower !==' ' && high !==' '){
+        if(lower !== high){
             return false;
         }
+    }
         start++;
         end--;
     }
     return true;
 }
 let str="raceaecar";
-console.log(twopointerpalindrom(str));
+let str1="Race aecar";
+// console.log(twopointerpalindrom(str));
+console.log(twopointerpalindrom(str1));
+
+// This two-pointer method is efficient (O(n) time, O(1)
+//  space) and a standard technique seen in many 
+//  palindrome-checking tutorials 
