f99f3a23bbabc0597810b3d12d8a6894a3ea65d1
Priyanka Mehta <primehta17@gmail.com>
2025-06-26 00:02:15 +0530

    palindrometwopointer

---
+++ mediumquestion/palindrometwopointer.js
@@ -0,0 +1,14 @@
+function twopointerpalindrom(str){
+    let start=0;
+    let end=str.length-1;
+    while(start<end){
+        if(str[start] !== str[end]){
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true;
+}
+let str="raceaecar";
+console.log(twopointerpalindrom(str));
