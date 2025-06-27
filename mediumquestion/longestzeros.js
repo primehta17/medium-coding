bc9a2b4bf082c6a766f92affcfaa8cebdc4573ff
Priyanka Mehta <primehta17@gmail.com>
2025-06-28 00:02:59 +0530

    longestzeros

--- mediumquestion/longestzeros.js
+++ mediumquestion/longestzeros.js
@@ -1,18 +1,19 @@
 function longestConsecutiveZeros(str) {
     let current=0;let max=0;
     for(let i=0;i<str.length;i++){
-        if(str[i]=='0'){
+        if(str[i]==='0'){
           current++;
         }else{
-           if(max<current){
+           if(max<current)
             max=current;
-            current=0
-           }
+            current=0;
+           
         } 
     }
-    if(max<current){
+    if(max<current)
         max=current;
-    }
+    
     return max;
 }
-console.log(longestConsecutiveZeros("1010010001"))
+console.log(longestConsecutiveZeros("1010010001"));
+console.log(longestConsecutiveZeros("10101010"));
