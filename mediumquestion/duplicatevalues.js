245816be7d6cf3e88deafe11d1349e3ed6d441c1
Priyanka Mehta <primehta17@gmail.com>
2025-06-30 22:58:15 +0530

    duplicatevalues

--- mediumquestion/duplicatevalues.js
+++ mediumquestion/duplicatevalues.js
@@ -14,11 +14,35 @@ function duplicateval(arr){
    let newarr=[];
     for( let t in seen){
         if((seen[t])===1){
-            newarr.push(t);
+            newarr.push(Number(t));
         };
     }
   return newarr;
 }
+
 let arr=[7,2,2,3,3,9];
-// console.log(duplicateval(arr));
+console.log(duplicateval(arr));
+function set(arr){
+  let set = new Set(arr);
+  console.log([...set])
+}
+set(arr);
+
+
+let arr1=[7,2,2,3,3,9];
+
+function uniqueValue(arr){
+    let seen= new Set();
+    let duplicate = new Set();
+
+    for(let val of arr){
+        if(seen.has(val)){
+            duplicate.add(val);
+        }else{
+            seen.add(val);
+        }
+    }
+    return [...seen].filter(val=>!duplicate.has(val));
+}
+console.log(uniqueValue(arr1))
 
