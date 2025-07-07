53fa848a275e88a1da104c981e40b3a49fbf3420
Priyanka Mehta <primehta17@gmail.com>
2025-07-07 23:03:58 +0530

    recusion sum array

--- recursion/sumarray.js
+++ recursion/sumarray.js
@@ -1,9 +1,10 @@
 function sumarray(n){
-    if(n==0){
-        return arr[0];
-    }
-    return arr[n] + sumarray(n-1);
+   if(n==0){
+    return arr[0];
+   }
+   return arr[n] + sumarray(n-1);
 }
-let arr=[1,2,3,4,5];
+let arr=[21,12,13,14,15];
 let len=arr.length-1;
 console.log(sumarray(len));
+
