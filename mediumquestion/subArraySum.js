383ff4e270c61aee46298198d673f4368dca3194
Priyanka Mehta <primehta17@gmail.com>
2025-07-09 02:15:56 +0530

    subArraySum

--- mediumquestion/subArraySum.js
+++ mediumquestion/subArraySum.js
@@ -1,8 +1,12 @@
 function subArraySum(arr) {
     let sum=0;
     for(let i=0;i<arr.length;i++){
-        let mul=arr[i]*(i+1)*(arr.length-i);
-        sum+=mul;
+        let mul=(i+1)*(arr.length-i);
+       
+        sum+=arr[i]*mul;
+       //also can write like this
+         //let mul=arr[i]*(i+1)*(arr.length-i);
+          //sum+=arr[i];
     }
     return sum;
 }
