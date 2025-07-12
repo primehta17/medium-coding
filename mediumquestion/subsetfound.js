4be540154e08fd0da055e8028572ba315faba78d
Priyanka Mehta <primehta17@gmail.com>
2025-07-12 23:20:49 +0530

    subset foundfor

--- mediumquestion/subsetfound.js
+++ mediumquestion/subsetfound.js
@@ -7,3 +7,21 @@ function isSubset(arr1, arr2) {
      }
 }
 console.log(isSubset([4,6,7], [4,6,7,5,2]));
+
+function subsetfound(arr1,arr2){
+    for(let i=0;i<arr1.length;i++){
+        let item=arr1[i];let found=false;
+
+        for(let j=0;j<arr2.length;j++){
+            if(item===arr2[j]){
+                 found=true;
+               break;
+            }
+        }
+        if(!found){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(subsetfound([4,6,7], [4,6,7,5,2]));
