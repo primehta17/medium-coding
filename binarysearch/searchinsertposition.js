49027d57daf27032c04fc6d83e9e21e6b09908bb
Priyanka Mehta <primehta17@gmail.com>
2025-07-15 00:00:24 +0530

    search insert position

---
+++ binarysearch/searchinsertposition.js
@@ -0,0 +1,18 @@
+function searchinsertposition35(arr,target){
+  let first=0;
+  let last =arr.length-1;
+  while(first<=last){
+    let mid=Math.floor((first+last)/2);
+    if(arr[mid]==target){
+        return mid;
+    }else if(arr[mid]<target){
+      first=  mid+1;
+    }else{
+    last = mid-1;
+    }
+  }
+return -1;
+
+
+}
+console.log(searchinsertposition35([1,3,5,6],5))
