1038357858d9489a0ac5c0f99fa112bc7e0c943c
Priyanka Mehta <primehta17@gmail.com>
2025-07-27 00:26:52 +0530

    doubleFactorial

--- numberquestion/doubleFactorial.js
+++ numberquestion/doubleFactorial.js
@@ -4,3 +4,13 @@ function doubleFactorial(n) {
     }
     return n* doubleFactorial(n-2);
 }
+console.log(doubleFactorial(7));// n!! = 1 * 3 * 5 * 7;
+console.log(doubleFactorial(5));
+
+let m=7;
+
+let double=1;
+for(let i=m;i>=0;i-=2){
+    double*=i;
+}
+console.log(double)
