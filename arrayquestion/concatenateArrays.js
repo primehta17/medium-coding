43d334747a0b1d07256a9740f99a282f3e3986cc
Priyanka Mehta <primehta17@gmail.com>
2025-07-28 23:48:28 +0530

    concatenateArrays

--- arrayquestion/concatenateArrays.js
+++ arrayquestion/concatenateArrays.js
@@ -1,4 +1,12 @@
 function concatenateArrays(arr1, arr2) {
-    return [...arr1,arr2].sort((a,b)=>a-b);
+    // return [...arr1,arr2].sort((a,b)=>a-b);
+let newarr=[];
+    for(let i=0;i<arr1.length;i++){
+        newarr.push(arr1[i]);
+        newarr.push(arr2[i]);
+    }
+    return newarr.sort((a,b)=>a-b);
 }
-console.log(concatenateArrays([1,2,3],[4,5,6]))
+console.log(concatenateArrays([1,2,3],[4,5,6]));
+
+
