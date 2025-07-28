8fb1b859b8f9913f9d7701b09e74ddde37c6acb8
Priyanka Mehta <primehta17@gmail.com>
2025-07-29 00:09:23 +0530

    concatenateArrays

--- arrayquestion/concatenateArrays.js
+++ arrayquestion/concatenateArrays.js
@@ -9,4 +9,21 @@ let newarr=[];
 }
 console.log(concatenateArrays([1,2,3],[4,5,6]));
 
+function mergeSorted(arr1,arr2){
+    const result=[];
+    let i=0,j=0;
+    while(i< arr1.length && j< arr2.length){
+        if(arr1[i]<= arr2[j]){
+            result.push(arr1[i++]);
+        }else{
+            result.push(arr2[j++]);
+        }
+    }
+    while(i<arr1.length) result.push(arr1[i++]);
+    while(j<arr2.length) result.push(arr2[j++]);
+
+    return result;
+}
+console.log(mergeSorted([7,9,10],[11,12,13]))
+
 
