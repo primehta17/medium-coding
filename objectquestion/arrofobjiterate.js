4601f2649204657fef0d5b60179583b6fd1eea4f
Priyanka Mehta <primehta17@gmail.com>
2025-07-30 22:16:39 +0530

    arrofobjiterate

--- objectquestion/arrofobjiterate.js
+++ objectquestion/arrofobjiterate.js
@@ -1,9 +1,20 @@
 function arrayofobject(arr){
-    for(let i=0;i<arr.length;i++){
-      for(let j in arr[i]){
-        console.log(j)
-        console.log(arr[i][j])
+  let keys=[],values=[];
+    for(let i of arr){
+      for(let j in i){
+      keys.push(j)
+        values.push(i[j])
       }
     }
+    return [keys,values]
 }
-arrayofobject([{1:"a",2:"b"},{3:"c",4:"d"}]);
+let arr=[{1:"a",2:"b"},{1:"c",2:"d"}];
+console.log(arrayofobject(arr));
+
+arr.forEach(val=>{
+  console.log(val[1])
+})
+
+
+const map=arr.map(u=>u[2]);
+console.log(map)
