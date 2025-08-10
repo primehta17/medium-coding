be77e00841b2fbca7079f8ad4769e63799bb9978
Priyanka Mehta <primehta17@gmail.com>
2025-08-11 00:10:26 +0530

    calculateDivisibilityScore

--- numberquestion/calculateDivisibilityScore.js
+++ numberquestion/calculateDivisibilityScore.js
@@ -9,3 +9,21 @@ function calculateDivisibilityScore(num) {
 }
 console.log(calculateDivisibilityScore(24));
 console.log(calculateDivisibilityScore(12));
+
+function countDivisorsOptimized(n) {
+  let count = 0;
+  const limit = Math.sqrt(n);
+  for (let i = 1; i <= limit; i++) {
+    if (n % i === 0) {
+      // i is a divisor
+      count++;
+      // Check if the complementary divisor is different
+      if (i !== n / i) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+console.log(countDivisorsOptimized(12));
