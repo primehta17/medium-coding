dfadc03fd47d4f57486057653f6c8388be74d843
Priyanka Mehta <primehta17@gmail.com>
2025-08-10 23:44:48 +0530

    calculateDivisibilityScore

--- numberquestion/calculateDivisibilityScore.js
+++ numberquestion/calculateDivisibilityScore.js
@@ -1,6 +1,6 @@
 function calculateDivisibilityScore(num) {
-  let count = 1;
-  for (let i = 0; i < num; i++) {
+  let count = 0;
+  for (let i = 1; i <= num; i++) {
     if (num % i == 0) {
       count++;
     }
