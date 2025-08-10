4a029d335ed00dffcc1331b7422aaeb9d429da99
Priyanka Mehta <primehta17@gmail.com>
2025-08-10 23:28:27 +0530

    swap two char double

---
+++ stringquestion/swaptwochar.js
@@ -0,0 +1,14 @@
+function doubleCharacterSwap(str, c1, c2) {
+  let newstr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === c1) {
+      newstr += c2;
+    } else if (str[i] === c2) {
+      newstr += c1;
+    } else {
+      newstr += str[i];
+    }
+  }
+  return newstr;
+}
+console.log(doubleCharacterSwap("aabbcc", "a", "c"));
