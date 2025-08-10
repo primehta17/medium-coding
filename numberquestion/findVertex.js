e93eff523bd7b3f69c89c5c4f86d59fb63526b08
Priyanka Mehta <primehta17@gmail.com>
2025-08-11 00:20:30 +0530

    find vertex

---
+++ numberquestion/findVertex.js
@@ -0,0 +1,10 @@
+function quadric(x, a, b, c) {
+  return a * x * x + b * x + c;
+}
+
+function findVertex(a, b, c) {
+  let h = -b / (2 * a);
+  let k = quadric(h, a, b, c);
+  return [h, k];
+}
+console.log(findVertex(1, -3, 2));
