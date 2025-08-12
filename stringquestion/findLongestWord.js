e2ef7caea67a3bbc0ed184cc78fa90de1578ce94
Priyanka Mehta <primehta17@gmail.com>
2025-08-13 02:13:25 +0530

    findLongestWordregex

--- stringquestion/findLongestWord.js
+++ stringquestion/findLongestWord.js
@@ -13,3 +13,20 @@ function findLongestWord(sentence) {
 }
 console.log(findLongestWord("The quick brown fox jumps over the lazy dog"));
 console.log(findLongestWord("Hello world! I am a programmer."));
+
+function findLongestWordRegex(sentence) {
+  let split = sentence.split(" ");
+  let max = split[0].length;
+  let result = "";
+  for (let i = 0; i < split.length; i++) {
+    let splitreplace = split[i].replace(/[^a-zA-Z]/g, "");
+    if (max < splitreplace.length) {
+      max = splitreplace;
+      result = split[i];
+    }
+  }
+  return result;
+}
+
+console.log(findLongestWordRegex("The weather is really nice today."));
+console.log(findLongestWordRegex("This is a test sentence."));
