d07146b741a6f8143c28d2d53a6a993506ac344f
Priyanka Mehta <primehta17@gmail.com>
2025-08-12 22:11:44 +0530

    maxWordsCanType

---
+++ numberquestion/maxWordsCanType.js
@@ -0,0 +1,18 @@
+function maxWordsCanType(str, brokenLetter) {
+  str = str.toLowerCase();
+  let split = str.split(" ");
+  let letter = [];
+  for (let i = 0; i < split.length; i++) {
+    for (let j = 0; j < split[i].length; j++) {
+      if (split[i][j] === brokenLetter) {
+        letter.push(split[i]);
+      }
+    }
+  }
+  // return letter;
+  return Math.abs(letter.length - split.length);
+}
+console.log(maxWordsCanType("I am a programmer", "a"));
+console.log(maxWordsCanType("hello world", "d"));
+console.log(maxWordsCanType("broken keyboard challenge", "b"));
+console.log(maxWordsCanType("Coding in JavaScript", "c"));
