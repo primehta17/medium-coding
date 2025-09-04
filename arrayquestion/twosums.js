c9e6d44227164f1b16fa070f8ef39144be487676
Priyanka Mehta <primehta17@gmail.com>
2025-09-04 13:45:28 +0530

    two sums map

--- arrayquestion/twosums.js
+++ arrayquestion/twosums.js
@@ -11,3 +11,17 @@ function twosums(arr, target) {
 }
 console.log(twosums([2, 3, 11, 15], 13));
 console.log(twosums([10, 20, 30, 40], 50));
+
+function twoSumsrefactor(nums, target) {
+  let numMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    let compliment = target - nums[i];
+    if (compliment in numMap && numMap[compliment] !== i) {
+      return [numMap[compliment], i];
+    }
+    numMap[nums[i]] = i;
+  }
+  return [];
+}
+console.log(twoSumsrefactor([2, 3, 11, 15], 13));
+console.log(twoSumsrefactor([10, 20, 30, 40], 50));
