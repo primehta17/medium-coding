f04c94f642de903cc99f696a2ad305f0cfbdeb99
Priyanka Mehta <primehta17@gmail.com>
2025-09-04 18:38:42 +0530

    two sums map

--- arrayquestion/twosums.js
+++ arrayquestion/twosums.js
@@ -25,3 +25,21 @@ function twoSumsrefactor(nums, target) {
 }
 console.log(twoSumsrefactor([2, 3, 11, 15], 13));
 console.log(twoSumsrefactor([10, 20, 30, 40], 50));
+
+function twoSums(arr, target) {
+  let map = {};
+  // Build mapping to last index of each number
+  for (let i = 0; i < arr.length; i++) {
+    map[arr[i]] = i;
+  }
+  // Iterate again to find the match with the last possible index
+  for (let i = 0; i < arr.length; i++) {
+    let complement = target - arr[i];
+    if (map[complement] !== undefined && map[complement] !== i) {
+      return [i, map[complement]];
+    }
+  }
+  return [];
+}
+console.log(twoSums([2, 3, 11, 15], 13));
+console.log(twoSums([10, 20, 30, 40], 50));
